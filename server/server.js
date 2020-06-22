2e446ada9022981e8db913a59b852bbd84b92f54
Tecnico2011 <garciafebresd@gmail.com>
2020-06-22 10:32:21 -0400

    first commit: rest server practice

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.json('Hello World');
+});
+
+app.listen(3000, () => {
+
+    console.log('listen port 3000');
+
+});
