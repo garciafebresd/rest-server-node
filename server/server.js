4b31ade71f47e4199296a1b51754dd6e3f2fa962
Tecnico2011 <garciafebresd@gmail.com>
2020-06-24 07:38:51 -0400

    adding some request

--- server/server.js
+++ server/server.js
@@ -1,12 +1,46 @@
-const express = require('express');
-const app = express();
+require('./config/config');
 
-app.get('/', function(req, res) {
-    res.json('Hello World');
+const express = require("express");
+const app = express();
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/usuario", function(req, res) {
+    res.json("get usuario");
 });
 
-app.listen(3000, () => {
+app.post("/usuario", function(req, res) {
+    const usuario = req.body;
 
-    console.log('listen port 3000');
+    if (usuario.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            message: 'El parametro nombre obligatorio'
+        });
+    }
 
+    res.json({
+        usuario
+    });
+});
+
+app.put("/usuario:id", function(req, res) {
+    const id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+
+app.delete("/usuario", function(req, res) {
+    res.json("delete usuario");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('listen port ' + process.env.PORT);
 });
