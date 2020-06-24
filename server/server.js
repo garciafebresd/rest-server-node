f1670f7d8181f6e093e0c9336c7281e400d7958d
Tecnico2011 <garciafebresd@gmail.com>
2020-06-24 09:47:28 -0400

    adding user routes file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require("express");
+const mongoose = require('mongoose');
+
 const app = express();
 var bodyParser = require('body-parser');
 
@@ -8,37 +10,18 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get("/usuario", function(req, res) {
-    res.json("get usuario");
-});
+app.use(require('./routes/usuario'));
 
-app.post("/usuario", function(req, res) {
-    const usuario = req.body;
+mongoose.connect('mongodb://localhost:27017/cafe', (err, resp) => {
 
-    if (usuario.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            message: 'El parametro nombre obligatorio'
-        });
+    if (err) {
+        throw err;
     }
 
-    res.json({
-        usuario
-    });
-});
+    console.log('Base de datos ONLINE');
 
-app.put("/usuario:id", function(req, res) {
-    const id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete("/usuario", function(req, res) {
-    res.json("delete usuario");
 });
 
 app.listen(process.env.PORT, () => {
